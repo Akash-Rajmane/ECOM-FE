bd27712f8d7107860c90807c7caeeafc8587a0ef
Akash <akashrajmane23@gmail.com>
2024-06-09 09:43:08 +0530

    minor change

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -6,7 +6,7 @@ const AuthForm = ({ auth }) => {
     const authMode = auth.mode === "login" ? "Log In" : "Sign Up";
 
     return (
-        <Form noValidate validated={auth.validated} onSubmit={auth.mode === "login" ? auth.logInHandler : auth.signUpHandler} className='d-flex flex-column justify-content-center align-items-center w-75 gap-3'>
+        <Form  validated={auth.validated} onSubmit={auth.mode === "login" ? auth.logInHandler : auth.signUpHandler} className='d-flex flex-column justify-content-center align-items-center w-75 gap-3'>
             <h3 className='text-primary fw-bolder fs-2'>{authMode}</h3>
             {auth.mode !== "login" && <Form.Control type="text" value={auth.name} onChange={auth.nameChangeHandler} placeholder="Name" pattern="[A-Za-z ]{4,32}" title={'Must be between 4 to 32 letters, capital or small'} required />}
             <Form.Control type="email" value={auth.email} onChange={auth.emailChangeHandler} placeholder="Email" required />
