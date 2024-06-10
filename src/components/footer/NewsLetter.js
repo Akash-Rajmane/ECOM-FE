62b923e33f4480464cb2b2a95056592ffe3faa7c
Akash <akashrajmane23@gmail.com>
2024-06-10 18:23:09 +0530

    minor changes

--- src/components/footer/NewsLetter.js
+++ src/components/footer/NewsLetter.js
@@ -7,8 +7,8 @@ import Form from 'react-bootstrap/Form';
 const NewsLetter = () => {
   return (
     <section className='w-100'>
-        <h4>NEWSLETTER</h4>
-        <p className='my-3 fw-medium'>Sign Up for Our Newsletter</p>
+        <h4 className='text-center'>NEWSLETTER</h4>
+        <p className='my-3 fw-medium text-center'>Sign Up for Our Newsletter</p>
         <div className="my-2 d-flex gap-2 justify-content-center">
             <Form.Control
             placeholder='Enter Your Email'
